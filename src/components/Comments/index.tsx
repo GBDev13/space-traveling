dd108eb3566ae1ec01b7dbc8b7bcb50fbbc6b034
Gabriel Borges <gbdesigns13@gmail.com>
2021-04-03 20:13:23 -0300

    Changed repo name in comments 🚀💜

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -9,10 +9,7 @@ export default class Comments extends Component {
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
     script.setAttribute('async', 'true');
-    script.setAttribute(
-      'repo',
-      'GBDev13/ignite-desafio-reactjs-criando-um-projeto-do-zero'
-    );
+    script.setAttribute('repo', 'GBDev13/space-traveling');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('theme', 'dark-blue');
     anchor.appendChild(script);
